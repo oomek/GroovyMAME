43b9a9fb59cd6b6439b04508830cf9a3dec68c3c
Antonio Giner <calamity15khz@gmail.com>
2020-03-30 13:39:55 +0200

    Make extract_video_config public in SDL so we can call it

--- src/osd/sdl/osdsdl.h
+++ src/osd/sdl/osdsdl.h
@@ -158,6 +158,8 @@ public:
 	virtual void video_exit() override;
 	virtual void window_exit() override;
 
+	void extract_video_config();
+
 	// SDL-specific
 	virtual bool has_focus() const override { return bool(m_focus_window); }
 	void poll_inputs();
@@ -187,7 +189,6 @@ private:
 
 	virtual void osd_exit() override;
 
-	void extract_video_config();
 	void output_oslog(const char *buffer);
 
 	void process_window_event(SDL_Event const &event);
