5cb10e7623a33720bdb56056216f00ae2c225ed1
Antonio Giner <calamity15khz@gmail.com>
2021-01-03 17:37:17 +0100

    Don't call update_resolution in Windows's osd because WM_DISPLAYCHANGE only returns resolution for primary monitor. monitor->refresh() alone does the job.

--- src/osd/windows/window.cpp
+++ src/osd/windows/window.cpp
@@ -1396,7 +1396,6 @@ LRESULT CALLBACK win_window_info::video_window_proc(HWND wnd, UINT message, WPAR
 		 * should be used.
 		 */
 		window->monitor()->refresh();
-		window->monitor()->update_resolution(LOWORD(lparam), HIWORD(lparam));
 		break;
 
 	// set focus: if we're not the primary window, switch back
