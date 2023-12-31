a7c0c96cde6b1af832932c0066f9b7b676fab0e8
Antonio Giner <calamity15khz@gmail.com>
2023-12-31 12:49:36 +0100

    [nogpu] First release
    - Add support for Linux
    - Make sleeping optional by linking it to option -sleep
    - Add option -mister_fd_margin, in ms, to set a secury margin for automatic frame delay, in ms. Default is 1.5 ms

--- scripts/src/osd/modules.lua
+++ scripts/src/osd/modules.lua
@@ -544,6 +544,10 @@ function osdmodulestargetconf()
 		end
 	end
 
+	links {
+			"lz4",
+		}
+
 	if _OPTIONS["targetos"]=="windows" then
 		links {
 			"gdi32",
@@ -552,7 +556,6 @@ function osdmodulestargetconf()
 			"oleaut32",
 			"winmm",
 			"hid",
-			"lz4",
 		}
 	elseif _OPTIONS["targetos"]=="macosx" then
 		links {
