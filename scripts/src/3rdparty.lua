8223b3a738ee8478b11dfa56774c555645161223
Antonio Giner <calamity15khz@gmail.com>
2024-01-17 23:02:22 +0100

    Fix Mac build, finally.

--- scripts/src/3rdparty.lua
+++ scripts/src/3rdparty.lua
@@ -2177,12 +2177,6 @@ if _OPTIONS["targetos"]=="linux" then
 		backtick("pkg-config --cflags sdl2"),
 	}
 
-	if _OPTIONS["gcc"]~=nil and string.find(_OPTIONS["gcc"], "clang") then
-		buildoptions {
-			"-Wno-unused-private-field",
-		}
-	end
-
 	links {
 		"Xrandr",
 	}
@@ -2190,4 +2184,10 @@ if _OPTIONS["targetos"]=="linux" then
 	local str = backtick("pkg-config --libs libdrm")
 	addlibfromstring(str)
 	addoptionsfromstring(str)
+end
+
+if _OPTIONS["gcc"]~=nil and string.find(_OPTIONS["gcc"], "clang") then
+	buildoptions {
+		"-Wno-unused-private-field",
+	}
 end
