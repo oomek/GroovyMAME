decb71465d1b69f63c8c62ed32e0390cae83114d
Antonio Giner <calamity15khz@gmail.com>
2024-01-17 21:50:19 +0100

    [nogpu] Various fixes to make Mac build possible (audio included) [djfumberger]

--- scripts/src/3rdparty.lua
+++ scripts/src/3rdparty.lua
@@ -2203,6 +2203,12 @@ if _OPTIONS["targetos"]=="linux" then
 		backtick("pkg-config --cflags sdl2"),
 	}
 
+	if _OPTIONS["gcc"]~=nil and string.find(_OPTIONS["gcc"], "clang") then
+		buildoptions {
+			"-Wno-unused-private-field",
+		}
+	end
+
 	links {
 		"Xrandr",
 	}
