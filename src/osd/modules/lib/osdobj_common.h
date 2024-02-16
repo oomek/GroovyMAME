8a6702f43e622dd80f531993d517d775abbe4498
Antonio Giner <calamity15khz@gmail.com>
2024-02-16 17:35:28 +0100

    Add option -scale_proportional.

--- src/osd/modules/lib/osdobj_common.h
+++ src/osd/modules/lib/osdobj_common.h
@@ -123,6 +123,7 @@
 #define OSDOPTION_AUTOSYNC              "autosync"
 #define OSDOPTION_AUTOFILTER            "autofilter"
 #define OSDOPTION_AUTOSTRETCH           "autostretch"
+#define OSDOPTION_SCALE_PROPORTIONAL    "scale_proportional"
 #define OSDOPTION_DRI_DEVICE            "dri_device"
 #define OSDOPTION_SCREEN_COMPOSITING    "screen_compositing"
 #define OSDOPTION_SCREEN_REORDERING     "screen_reordering"
@@ -208,6 +209,7 @@ public:
 	bool autosync() const { return bool_value(OSDOPTION_AUTOSYNC); }
 	bool autofilter() const { return bool_value(OSDOPTION_AUTOFILTER); }
 	bool autostretch() const { return bool_value(OSDOPTION_AUTOSTRETCH); }
+	bool scale_proportional() const { return bool_value(OSDOPTION_SCALE_PROPORTIONAL); }
 	const char *dri_device() const { return value(OSDOPTION_DRI_DEVICE); }
 	bool screen_compositing() const { return bool_value(OSDOPTION_SCREEN_COMPOSITING); }
 	bool screen_reordering() const { return bool_value(OSDOPTION_SCREEN_REORDERING); }
